11e51926976e4cbe43cc569ad6bbe952b683f4ab
IamGideonIdoko <iamgideonidoko@gmail.com>
2022-03-14 14:30:43 +0100

    Setup editor

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+function handleEditorChange({ html, text }) {
+  console.log('handleEditorChange', html, text);
+}
+
+const App = () => {
+  return (
+    <div>
+      <MdEditor
+        style={{ height: '500px' }}
+        renderHTML={(text) => mdParser.render(text)}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
+};
+
+export default App;
--- index.tsx
+++ index.tsx
@@ -1,31 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
-
-interface AppProps { }
-interface AppState {
-  name: string;
-}
-
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
+import App from './App';
 
 render(<App />, document.getElementById('root'));
